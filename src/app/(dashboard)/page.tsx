023122a343f85fd28529f0c091ff4b40bd1e5ccc
huy <danghuy9229@gmail.com>
2024-07-08 14:49:30 +0700

    commit model

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,15 @@
 import { CourseGrid } from "@/components/common";
 import CourseItem from "@/components/course/CourseItem";
 import Heading from "@/components/Typography/Heading";
+import createUser from "@/lib/actions/user.actions";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await createUser({
+    clerkId: "clerk_123",
+    username: "test",
+    email_address: "test@gmail.com",
+  });
   return (
     <div>
       <Heading>Khám phá</Heading>
