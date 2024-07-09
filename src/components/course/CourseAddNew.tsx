2ce213661ed86f60555e6a32c9d39a9b5f9fdc3d
huy <danghuy9229@gmail.com>
2024-07-09 09:22:46 +0700

    Custom form, input, button UI. Install slugify

--- src/components/course/CourseAddNew.tsx
+++ src/components/course/CourseAddNew.tsx
@@ -8,56 +8,94 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { title } from "process";
+import slugify from "slugify";
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  title: z.string().min(10, "Tên khóa học phải có ít nhất 10 kí tự"),
+  slug: z.string().optional(),
 });
 
 function CourseAddNew() {
   // 1. Define your form.
+  const [isSubmitting, setisSubmitting] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      title: "",
+      slug: "",
     },
   });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    try {
+      setisSubmitting(true);
+      const data = {
+        title: values.title,
+        slug:
+          values.slug ||
+          slugify(values.title, {
+            lower: true,
+            locale: "vi",
+          }),
+      };
+      console.log(data);
+      // Call your API here.
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setisSubmitting(false);
+    }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+      <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="off">
+        <div className="grid grid-cols-2 gap-8 mt-10 mb-8 sm:flex sm:flex-col sm:gap-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tên khóa học *</FormLabel>
+                <FormControl>
+                  <Input placeholder="Tên khóa học" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="slug"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Đường dẫn khóa học</FormLabel>
+                <FormControl>
+                  <Input placeholder="khoa-hoc-lap-trinh" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          isLoading={isSubmitting}
+          variant="primary"
+          type="submit"
+          className="w-[120px]"
+          disabled={isSubmitting}
+        >
+          Tạo khóa học
+        </Button>
       </form>
     </Form>
   );
